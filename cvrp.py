403e4cf7a59be6a8314b4ba580eca6445b0f06f8
valentecaio <caio.valente@kbrwadventure.com>
2019-04-22 15:30:57 -0300

    remove some prints

--- cvrp.py
+++ cvrp.py
@@ -38,10 +38,6 @@ def generate_initial_solution(nodes, capacity):
   # initial state
   state = State([node.id for node in nodes[1:]])
 
-  print("----- INITIAL STATE -----")
-  print_nodes(nodes)
-  print_state(state)
-
   while len(state.clients_to_visit) > 0:
     # create a truck to visit nodes that have not been visited before
     truck = Truck(len(state.trucks), capacity, depot)
@@ -83,9 +79,6 @@ def generate_initial_solution(nodes, capacity):
       # stop truck looping when truck has returned to depot
       if truck.position == depot: break
   
-  print("----- FINAL STATE -----")
-  print_state(state)
-
   # convert state to an initial solution in the desired format
   # ex: [0, 21, 16, 18, 25, 5, 4, 29, 8, 11, 0, 28, 26, 12, 23, 7, 6, 0, 22, 9, 13, 17, 30, 3, 2, 0, 24, 19, 1, 15, 14, 10, 20, 0, 27, 0]
   initial_solution = [0]
@@ -93,7 +86,6 @@ def generate_initial_solution(nodes, capacity):
     for client_id in truck.route[1:]:
       initial_solution.append(client_id)
 
-  print("initial_solution: %s" % initial_solution)
   return initial_solution
 
 ### MAIN ###
@@ -102,9 +94,11 @@ def main():
   algorithm, filepath, verbose, cli_capacity = parse_args()
   nodes, vrp_capacity = parse_vrp(filepath)
   capacity = cli_capacity if cli_capacity else vrp_capacity
-  print("Depot: %s" % nodes[0])
   print("Capacity Q: %s" % capacity)
+  print_nodes(nodes)
   initial_solution = generate_initial_solution(nodes, capacity)
+  print("Initial_solution: %s" % initial_solution)
+
   # plot.draw_initial_state(depot, nodes)
   # plot.draw_results(nodes, initial_solution)
   is_valid_solution(nodes, capacity, initial_solution)

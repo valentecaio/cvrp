ab70f198569f3c0f9600212e4b6c4367add4e0b4
valentecaio <caio.valente@kbrwadventure.com>
2019-04-22 16:03:59 -0300

    simplify initial solution generator

--- classes.py
+++ classes.py
@@ -16,37 +16,3 @@ class Node:
     # cast to int to avoid working with floats
     return int( ( (self.x - x)**2 + (self.y - y)**2 )**.5 )
 
-
-class Truck:
-  def __init__(self, id, capacity, position):
-    self.id = id
-    self.capacity = int(capacity)
-    self.position = position # position must be a Node object
-    self.route = [position.id]
-
-  def __repr__(self):
-    return "(truck %s - capacity: %s, route: %s)"\
-          % (self.id, self.capacity, self.route)
-
-  def __eq__(self, other):
-    return self.id == other.id
-
-  def move_to_node(self, node):
-    self.capacity -= node.demand
-    self.route.append(node.id)
-    self.position = node
-
-
-class State:
-  def __init__(self, clients_to_visit):
-    self.clients_to_visit = clients_to_visit
-    self.trucks = []
-    self.cost = 0
-
-  def __repr__(self):
-    return "(clients_to_visit %s, cost: %s)"\
-          % (self.clients_to_visit, self.cost)
-
-  def remove_client(self, id_to_remove):
-    if id_to_remove in self.clients_to_visit:
-      self.clients_to_visit.remove(id_to_remove)
--- cvrp.py
+++ cvrp.py
@@ -2,7 +2,6 @@
 
 from pprint import pprint
 from cli import parse_args, parse_vrp
-from classes import Truck, State
 import plot
 
 ### DEBUG FUNCTIONS ###
@@ -32,36 +31,37 @@ def is_valid_solution(nodes, capacity, solution):
   # all routes are below truck capacity, solution ok
   return True
 
+# greedy algorithm that generates a valid initial solution
 def generate_initial_solution(nodes, capacity):
   depot = nodes[0]
 
   # initial state
-  state = State([node.id for node in nodes[1:]])
+  clients_to_visit = [node.id for node in nodes[1:]]
+  route = [0]
+  total_cost = 0
 
-  while len(state.clients_to_visit) > 0:
+  while len(clients_to_visit) > 0:
     # create a truck to visit nodes that have not been visited before
-    truck = Truck(len(state.trucks), capacity, depot)
-    state.trucks.append(truck)
+    truck_capacity = capacity
+    truck_position = 0
 
     # truck looping
     while True:
       # calculate costs
       costs = {}
-      for client_id in state.clients_to_visit:
+      for client_id in clients_to_visit:
         client = nodes[client_id]
-        cost = truck.position.distance_to_node(client.x, client.y)
-        # print("The cost to move to client %s is %s" % (client.id, cost))
-        costs[client.id] = cost
+        cost = nodes[truck_position].distance_to_node(client.x, client.y)
+        costs[client_id] = cost
       # sort possibilities from smallest to biggest cost, [(client_id, cost)]
       costs = sorted(costs.items(), key = lambda kv: (kv[1], kv[0]))
-      # print("Sorted costs : %s " % costs)
 
       # choose next destination (next client or depot)
       while len(costs) > 0:
         # pick first element of list
         candidate_id, candidate_cost = costs[0]
         costs = costs[1:]
-        if nodes[candidate_id].demand <= truck.capacity:
+        if nodes[candidate_id].demand <= truck_capacity:
           # this is a good candidate, it will be the next node
           next_node = nodes[candidate_id]
           cost_to_next_node = candidate_cost
@@ -69,24 +69,20 @@ def generate_initial_solution(nodes, capacity):
       else:
         # if truck capacity is not enough for any client, return to depot
         next_node = depot
-        cost_to_next_node = truck.position.distance_to_node(depot.x, depot.y)
+        cost_to_next_node = nodes[truck_position].distance_to_node(depot.x, depot.y)
 
       # go to next node and update state
-      truck.move_to_node(next_node)
-      state.cost += cost_to_next_node
-      state.remove_client(next_node.id)
+      route.append(next_node.id)
+      truck_capacity -= next_node.demand
+      truck_position = next_node.id
+      total_cost += cost_to_next_node
+      if next_node.id in clients_to_visit:
+        clients_to_visit.remove(next_node.id)
 
       # stop truck looping when truck has returned to depot
-      if truck.position == depot: break
-  
-  # convert state to an initial solution in the desired format
-  # ex: [0, 21, 16, 18, 25, 5, 4, 29, 8, 11, 0, 28, 26, 12, 23, 7, 6, 0, 22, 9, 13, 17, 30, 3, 2, 0, 24, 19, 1, 15, 14, 10, 20, 0, 27, 0]
-  initial_solution = [0]
-  for truck in state.trucks:
-    for client_id in truck.route[1:]:
-      initial_solution.append(client_id)
+      if truck_position == depot.id: break
 
-  return initial_solution
+  return route
 
 ### MAIN ###
 

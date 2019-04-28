634eceece7ae7695c6f6ecd92832103282c23157
valentecaio <caio.valente@kbrwadventure.com>
2019-04-27 23:57:54 -0300

    implement naive initial solution generator

--- initial_solution.py
+++ initial_solution.py
@@ -1,5 +1,6 @@
 from classes import Route
 from copy import deepcopy
+import random
 
 # greedy algorithm that generates a valid initial solution
 def greedy(nodes, capacity, algorithm = 'default'):
@@ -33,6 +34,66 @@ def greedy(nodes, capacity, algorithm = 'default'):
         candidate_id, candidate_cost = costs[0]
         costs = costs[1:]
         if nodes[candidate_id].demand <= truck_capacity:
+          # this is a good candidate, it will be the next node
+          next_node = nodes[candidate_id]
+          cost_to_next_node = candidate_cost
+          break
+      else:
+        # if truck capacity is not enough for any client, return to depot
+        next_node = depot
+        cost_to_next_node = nodes[truck_position].distance_to_node(depot.x, depot.y)
+
+      # go to next node and update state
+      route.path.append(next_node.id)
+      route.cost += cost_to_next_node
+      truck_capacity -= next_node.demand
+      truck_position = next_node.id
+
+      if next_node.id in clients_to_visit:
+        clients_to_visit.remove(next_node.id)
+
+      # stop truck looping when truck has returned to depot
+      if truck_position == depot.id: break
+
+    # store route data
+    routes.append(route)
+
+  # format output according to algorithm
+  if algorithm == 'annealing':
+    return format_to_annealing(routes)
+  elif algorithm == 'local_search':
+    return format_to_local_search(routes)
+  else:
+    return routes
+
+
+# naive algorithm that generates a valid random initial solution
+def naive(nodes, capacity, algorithm = 'default'):
+  depot = nodes[0]
+
+  # initial state
+  clients_to_visit = [node.id for node in nodes[1:]]
+  routes = []
+
+  while len(clients_to_visit) > 0:
+    # create a truck to visit nodes that have not been visited before
+    truck_capacity = capacity
+    truck_position = 0
+    # create empty route starting in 0
+    route = Route([0], 0)
+ 
+    # truck looping
+    while True:
+      # choose next destination (next client or depot)
+      clients_buffer = deepcopy(clients_to_visit)
+      while len(clients_buffer) > 0:
+        # pick random element of bufffer
+        random_index = random.randint(0, len(clients_buffer)-1)
+        candidate_id = clients_buffer[random_index]
+        clients_buffer.remove(candidate_id)
+        candidate_node = nodes[candidate_id]
+        candidate_cost = nodes[truck_position].distance_to_node(candidate_node.x, candidate_node.y)
+        if candidate_node.demand <= truck_capacity:
           # this is a good candidate, it will be the next node
           next_node = nodes[candidate_id]
           cost_to_next_node = candidate_cost
